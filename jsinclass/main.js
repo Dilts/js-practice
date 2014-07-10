78514f4adfd053e1120953ad6a5131c4b75b5730
Brian Dilts <brianedilts@gmail.com>
2014-07-10 11:56:34 -0600

    almost done with Penguin to the moon

--- jsinclass/main.js
+++ jsinclass/main.js
@@ -219,35 +219,93 @@ var MoonGame = (function() {
 	var NUM_BIRDS = 20;
 	var MAX_TOP = 60;
 	var MAX_LEFT = 90;
+		
+
 		//Bird Constructor
 	var Bird = function() {
 
 	};
-
 	Bird.prototype.create = function() {
 			this.el = $('<i class="bird icon-twitter-bird"></i>');
 			this.el.css({
 				top: Math.random() * MAX_TOP + '%',
-				left: Math.random() * MAX_LEFT + '%'
+				left: Math.random() * MAX_LEFT + '%',
+				color: '#' + (Math.random().toString(16) + '000000').slice(2, 8)
 			})
 
 			return this.el;
 	};
 
+		Bird.prototype.addClickHandler = function () {
 
+		}
+
+		//penguin constructor
+		var Penguin = function() {
+		}
+		Penguin.prototype.create = function() {
+			this.el = $('<i class="penguin icon-plancast">');
+			return this.el;
+		}
+
+		// Flock constructor
+		var Flock = function(penguin) {
+			this.penguin = penguin;
+			this.birds = [];
+		}
+
+		Flock.prototype.create = function() {
+			var newEl = $('<div class="flock">');
+			newEl.append(this.penguin.create());
+			newEl.css('bottom', this.birds.length * 5)
+			
+			for(var i=0; i<this.birds.length; i++) {
+				newEl.append(this.birds[i].el)
+			}
+
+			if(this.el) {
+				this.el.replaceWith(newEl);
+			}
+			this.el = newEl;
+
+			return newEl;
+		}
+
+		Flock.prototype.addBirdClickHandler = function(bird) {
+			var self = this;
+			bird.el.on('click', function() {
+			self.birds.push(bird)
+			self.create()
+			})
+		}
 
 		//Declare array of free-flying birds
 	var birds = [];
 	var flock = null;
 
 
+		//Create the birds in the sky
 	var init = function() {
+
+		//create the flock
+		var penguin = new Penguin();
+		flock = new Flock(penguin);
+		$('.sky').append(flock.create());	
+
+
 		for(var i=0; i<NUM_BIRDS; i++) {
 			var bird = new Bird();
 			var birdEl = bird.create();
 			$('.sky').append(birdEl);
+			birds.push(bird);
+			flock.addBirdClickHandler(bird);
 		}	
+	
+
+
+	
 	}
+	
 
 	// Return an object literal with the properties and methods that
 	// we wish to "reveal" to the rest of the program.
