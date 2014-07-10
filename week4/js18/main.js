99a7c3740363235556b5e719737e8e15d4a18cb0
Brian Dilts <brianedilts@gmail.com>
2014-07-10 14:19:28 -0600

    added raty library

--- week4/js18/main.js
+++ week4/js18/main.js
@@ -16,16 +16,63 @@ var Quote = function(quote, author, rating) {
 var enterQuote = new Quote('Getting to the summit is optional, getting down is mandatory.', 'Ed Visters', 4);
 console.log(enterQuote)
 
+
+
+
+
+$('.submit').click(function() {
+	
+	var quoteContainer = $('<div></div>')
+	
+	$('#quote-log').append(quoteContainer)
+	quoteContainer.append("<p>Quote: " + $('.quote-input').val() + "</p>").append("<p>Author: " + $('.quote-author').val() + "</p>")
+	
+	$('.quote-input').val("")
+	$('.quote-author').val("")
+
+
+	})
+		$(function()
+		{
+			/*
+				Basic Example with custom image path
+				path : indicate custom path to the star image
+			*/
+			$('#star').raty({ path: 'http://wbotelhos.com/raty/lib/img/' });
+			
+			
+			/*
+				Predefined score with half star,
+				score : You can pass any value for it, not necessarily,
+						a data- value.
+						You can use a rating-score too.
+						but then 
+						code will be return $(this).attr('rating-score');
+				half 	: that will enable user to add half star rating
+				target  : define the rating hint container
+				targetKeep : this will allow used to
+					keep selected result inside hint container
+					defining false will loose selected value
+			*/
+
+			$('#predefined-star').raty(
+			{
+				path: 'http://wbotelhos.com/raty/lib/img/',
+				half: true,
+				target: '#result',
+				targetKeep: true,
+				score: function()
+				{
+					return $(this).attr('data-score');
+				}
+			});
+		});
+
 });
 
-
-
-
-
-
-
-
-// _.sortBy( QuoteArray)
+// _.sortBy( QuoteArray, function (item)) {
+// 		return item.rating
+// }
 
 
 
